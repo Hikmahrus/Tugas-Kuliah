6ae4b59cd7d6d90ad9510656b4f3801a77de5d8d
Hikmahrus Gunandaru <65547127+Hikmahrus@users.noreply.github.com>
2021-03-15 19:59:16 +0700

    Add files via upload

--- 20051204064_Prak ke 3_stack.cpp
+++ 20051204064_Prak ke 3_stack.cpp
@@ -1,118 +1,188 @@
 /*
-* DETAILS : 
-* 1. Queue max 10
-* 2. Plat nomor = string
-* 3. Delete queue by plat nomor (search plat nomor then delete it)
-* 4. 2 linked list
-TODO : Return data from stack to queue
-? jumlah mobil sudah diketahui atau jumlah mobil dihitung setiap saat ada mobil baru yang akan masuk
-! jumlah mobil sudah diketahui
+TODO : Fix BUG
+! ERORR !
 */
-
 #include <iostream>
+#include <climits>
 using namespace std;
 struct Node{
     string plate;
     Node* next;
+    Node* prev;
 };
 struct Structure{
     Node* head = new Node;
     Node* tail = new Node;
-    int limit = 1;
+    int limit = 10;
 };
 Structure queue;
 Structure stack;
-void initQueue(){
+void initQueue(int limit = 10){
     queue.head = NULL;
     queue.tail = NULL;
+    queue.limit = limit;
 }
-void initStack(){
+void initStack(int limit = 10){
     stack.head = NULL;
     stack.tail = NULL;
+    stack.limit = limit;
 }
 bool isEmpty(Structure strc){
     return (strc.head == NULL)? true : false;
 }
+bool isFull(Structure strc){
+    return (strc.limit == 0)? true : false;
+}
 void pushQueue(string str){
     Node* temp = new Node;
-    Node* check = new Node;
     temp->plate = str;
     temp->next = NULL;
     if(isEmpty(queue)){
         queue.head = temp;
         queue.tail = temp;
-        queue.limit++;
-    }else if(queue.limit <= 10) {
-        check = queue.head;
-        while(check->next != NULL){
-            check = check->next;
-        }
-        check->next = temp;
-        queue.tail = check->next;
-        queue.limit++;
+        queue.limit--;
+    }else if(!isFull(queue)) {
+        queue.tail->next = temp;
+        queue.tail = temp;
+        queue.limit--;
     } else {
         cout<<"Tempat parkir penuh"<<endl;
     }
 }
 void pushStack(Node* node){
-    Node* check = new Node;
+    Node* prev = new Node;
     Node* temp = new Node;
     temp->plate = node->plate;
     temp->next = NULL;
+    temp->prev = NULL;
     if(isEmpty(stack)){
         stack.head = temp;
-        stack.limit++;
-    } else if(stack.limit != 10){
-        check = stack.head;
-        while(check->next != NULL){
-            check = check->next;
-        }
-        check->next = temp;
-        stack.tail = check->next;
-        stack.limit++;
+        stack.tail = temp;
+        stack.limit--;
+    } else if(!isFull(stack)){
+        prev = stack.tail;
+        stack.tail->next = temp;
+        stack.tail = temp;
+        stack.tail->prev = prev;
+        stack.limit--;
     } else {
         cout<<"Plat nomor mobil tidak ditemukan"<<endl;
     }
 }
-void popQueue(string str){
+void popQueue(string str = "NULL"){
     Node* check = new Node;
     Node* pop = new Node;
     check = queue.head;
-    while(check->next != NULL){
-        if(check->plate == str){
-            cout<<"Mobil dengan plat nomor "<<str<<" telah keluar"<<endl;
-            pop = check;
-            check = check->next;
-            queue.head = check;
-            delete pop;
-            break;
-        }
-        pushStack(check);
+    if(isEmpty(queue)) {
+        cout<<"Tempat parkir kosong"<<endl;
+    } else if(str == "NULL"){
+        cout<<"Mobil dengan plat nomor "<<queue.head->plate<<" telah dikeluarkan"<<endl;
         pop = check;
         check = check->next;
         queue.head = check;
         delete pop;
+        queue.limit++;
+    } else {
+        while(check != NULL) {
+            if(check->plate == str){
+                cout<<"Mobil dengan plat nomor "<<str<<" telah keluar"<<endl;
+                pop = check;
+                check = check->next;
+                queue.head = check;
+                delete pop;
+                queue.limit++;
+                break;
+            } else {
+                pushStack(check);
+                pop = check;
+                check = check->next;
+                queue.head = check;
+                delete pop;
+            }
+        }
     }
 }
 void popStack(){
-    //* pop stack and insert to queue
-    //TODO: code here
+    Node* pop = new Node;
+    if(isEmpty(stack)){
+        cout<<"Stack is empty"<<endl;
+    } else {
+        while(!isEmpty(stack)){
+            if(stack.tail != stack.head){
+                stack.tail->next = queue.head;
+                queue.head = stack.tail;
+                stack.tail = stack.tail->prev;
+                stack.tail->next = NULL;
+                stack.limit++;
+            } else {
+                stack.tail->next = queue.head;
+                queue.head = stack.tail;
+                stack.limit++;
+                stack.head = NULL;
+                stack.tail = NULL;
+            }
+        }
+    }
 }
 void printKeyStack(){
-    cout<<"stack head value : "<<stack.head->plate<<endl;
-    cout<<"stack tail value : "<<stack.tail->plate<<endl;
+    if(isEmpty(stack)){
+        cout<<"stack empty"<<endl;
+    }else {
+        cout<<"stack head value : "<<stack.head->plate<<endl;
+        cout<<"stack tail value : "<<stack.tail->plate<<endl;
+    }
+    
 }
 void printKey(){
     cout<<"queue head value : "<<queue.head->plate<<endl;
     cout<<"queue tail value : "<<queue.tail->plate<<endl;
 }
-int main(){
-    initQueue();
-    initStack();
-    for(int i=1;i != 15;i++){
-        pushQueue(to_string(i));
+void printQueue(){
+    Node* check = new Node;
+    check = queue.head;
+    if(isEmpty(queue)){
+        cout<<"Tempat parkir kosong"<<endl;
+    } else {
+        while(check != NULL){
+            cout<<"plate in parking lot : "<<check->plate<<endl;
+            check = check->next;
+        }
     }
-    popQueue("8");
-    printKeyStack();
-    printKey();
+}
+int main(){
+    initQueue(5); //default = 10
+    initStack(5);
+    while(true){
+        int pilih;
+        string plat;
+        cout<<"1. Masukkan mobil\n2. Keluarkan mobil\n3. Keluarkan mobil berdasarkan nomor plat\n4. Tampilkan plat mobil\n5. Exit"<<endl;
+        cout<<"Pilih : ";
+        cin>>pilih;
+        cin.ignore();
+        switch(pilih){
+            case 1:
+                cout<<"Masukkan Plat Nomor Mobil : ";
+                cin>>plat;
+                pushQueue(plat);
+                break;
+            case 2:
+                popQueue();
+                break;
+            case 3:
+                cout<<"Masukkan Plat Nomor Mobil : ";
+                cin>>plat;
+                popQueue(plat);
+                popStack();
+                break;
+            case 4:
+                printQueue(); //change
+                break;
+            case 5:
+                return false;
+                break;
+            default:
+                cout<<"Input anda salah"<<endl;
+                break;
+        }
+    }
 }
